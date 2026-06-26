695104e68f175d75e389bc266c195eff3f3c1205
Tae.Garam (Office) <drasla217@gmail.com>
2026-06-26 09:39:59 +0900

    AdminUserCreate 작성 중간

--- app/(admin)/admin/users/users/create/index.tsx
+++
@@ -1,7 +0,0 @@
-import TextComponent from "@/components/common/text/TextComponent";
-
-function AdminUserCreatePage() {
-    return <TextComponent>create</TextComponent>;
-}
-
-export default AdminUserCreatePage;
